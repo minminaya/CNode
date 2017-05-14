05faea7edd86569cfd56395c7cec719f184860ff
Niwa <971382989@qq.com>
2017-05-14 17:37:37 +0800

    Commit-5，入手网络数据部分，首页model

---
+++ data/src/main/java/com/minminaya/data/api/CnodeApi.java
@@ -0,0 +1,8 @@
+package com.minminaya.data.api;
+
+/**
+ * Created by Niwa on 2017/5/14.
+ */
+
+public interface CnodeApi {
+}
---
+++ data/src/main/java/com/minminaya/data/http/NetWork.java
@@ -0,0 +1,10 @@
+package com.minminaya.data.http;
+
+/**
+ * Created by Niwa on 2017/5/14.
+ */
+
+public class NetWork {
+
+
+}
